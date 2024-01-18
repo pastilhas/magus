c7bcaabe29ef9b7cc6c6e7f6ea598641aaf62f62
Pastilhas <joao.campos1221@gmail.com>
2024-01-18 23:07:59 +0000

    add progress bar

--- src/assets/js/controls.js
+++ src/assets/js/controls.js
@@ -2,12 +2,14 @@ var player
 var time_played
 var time_left
 var play_btn
+var progress_bar
 
 window.addEventListener("load", (event) => {
     player = document.querySelector("#audio")
     time_played = document.querySelector("#played")
     time_left = document.querySelector("#left")
     play_btn = document.querySelector("#play_btn")
+    progress_bar = document.querySelector("#playtime")
 
     player.onplay = _ => { play_btn.innerText = 'PAUSE'; update_time() };
     player.onpause = _ => { play_btn.innerText = 'PLAY' };
@@ -43,6 +45,8 @@ function update_time_once() {
 
     time_played.innerText = format_time(played)
     time_left.innerText = format_time(left)
+
+    progress_bar.value = player.currentTime / player.duration
 }
 
 function update_time() {
